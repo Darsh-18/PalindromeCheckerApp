ca0e6e30fcc5773468343753753ae4cd8fb6afc1
chorariaom-ux <chorariaom@gmail.com>
2026-03-20 10:05:51 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Palindrome Checker App");
+
+        String input = "madam";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
     }
 }
